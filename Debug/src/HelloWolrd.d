df496d2ca61babe5bb998644058ed57ca1fe3fe8
nicolasarmenti <utnso@utnso.(none)>
2014-04-11 07:33:00 -0300

    Primer commit con un helo wolrd

---
+++ Debug/src/HelloWolrd.d
@@ -0,0 +1,1 @@
+src/HelloWolrd.d: ../src/HelloWolrd.c
---
+++ src/HelloWolrd.c
@@ -0,0 +1,15 @@
+/*
+ * HelloWolrd.c
+ *
+ *  Created on: 11/04/2014
+ *      Author: utnso
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Somos el grupo Commiteando por un Sueño");
+	return 0;
+}
