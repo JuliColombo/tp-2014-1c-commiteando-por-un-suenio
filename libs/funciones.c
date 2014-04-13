60322f79feb27596751f7b7ca6a186266f6edfe9
fedebonisconti <fedebonisconti26@gmail.com>
2014-04-12 23:19:46 -0300

    funciones peso y ordenar

---
+++ libs/funciones.c
@@ -0,0 +1,23 @@
+/*
+ * funciones.c
+ *
+ *  Created on: 12/04/2014
+ *      Author: utnso
+ */
+
+
+int peso (int cantEtiquetas, int cantFunc, int cantLineas){
+	return (5*cantEtiquetas + 3* cantFunc + cantLineas);
+}
+void ordenarPorPeso (int p, int n){
+	int i;
+	int max=p[0];
+
+	for(i=0;i<n;i++){
+		if (max < p[i]) {
+		max = p[i];
+		}
+	}
+}
+
+
