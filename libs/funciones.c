fc8820ce0003cee394f01163cdfce6cbf0c9ad18
fedebonisconti <fedebonisconti26@gmail.com>
2014-04-13 11:57:04 -0300

    estructura de programa

--- libs/funciones.c
+++ libs/funciones.c
@@ -5,11 +5,13 @@
  *      Author: utnso
  */
 
+#include "funciones.h"
 
-int peso (int cantEtiquetas, int cantFunc, int cantLineas){
+
+int peso (int cantEtiquetas, int cantFunc, int cantLineas){ //Calcula peso del programa
 	return (5*cantEtiquetas + 3* cantFunc + cantLineas);
 }
-void ordenarPorPeso (int p[], int n){
+void ordenarPorPeso (int p[], int n){      //Ordena la cola de new según el peso (no sé si ponerle que la cola ya están los pesos o para ordenarlos hay que calcular y ordenar en el momento)
 	int i;
 	int max=p[0];
 
@@ -20,4 +22,10 @@ void ordenarPorPeso (int p[], int n){
 	}
 }
 
+typedef struct { // se define un vector para los programas en new, y cuando el programa pasa de new a ready le asignamos el peso 0 (nunca van a tener peso 0)
+	int peso;
+	int cantEtiquetas;
+	int cantFunc;
+	int cantLineas;
 
+} programa;
