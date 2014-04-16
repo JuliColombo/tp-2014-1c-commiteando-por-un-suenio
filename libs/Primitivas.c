46284696de7fedc0467658d480fa0cc55b2b62c7
Fran Bravo <fran_ase@hotmail.com>
2014-04-16 18:49:53 -0300

    Agrego parte de las Primitivas

---
+++ libs/Primitivas.c
@@ -0,0 +1,37 @@
+/*
+ * Primitivas.c
+ *
+ *  Created on: 16/04/2014
+ *      Author: utnso
+ */
+
+#include <stdio.h>
+
+
+
+signed int* definirVariable(t_nombre_variable identificador_variable){ identificador_variable = malloc(sizeof(signed int));
+									if(identificador_variable*!=NULL){return identificador_variable*;}
+									else{ printf("No se puede reservar memoria");
+									return NULL*;
+												}
+}
+
+signed int* obtenerPosicionVariable(t_nombre_variable identificador_variable){
+}
+
+signed int dereferenciar(signed int* direccion_variable){signed int aux; //extrae el valor de la variable ubicada en direccion_variable
+														 aux=*direccion_variable;
+														 return aux;
+}
+
+void asignar(signed int* direccion_variable, signed int valor){ //copia el valor en la variable ubicada en direccion_variable
+	direccion_variable=&valor;
+}
+
+signed int obtenerValorCompartida(t_nombre_compartida variable){ //deberia solicitar el valor de la variable al kernel
+
+}
+
+signed int asignarValorCompartida(t_nombre_compartida variable, signed int valor){ //deberia solicitar al kernel que asigne valor a la variable
+
+}
