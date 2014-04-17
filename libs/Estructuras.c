085293996c9aa04f5c0089871848fce0bd16cbf8
AgustinVelurtas <agustinvelurtas@gmail.com>
2014-04-17 00:01:01 -0300

    Estructuras con PCB arreglado

---
+++ libs/Estructuras.c
@@ -0,0 +1,12 @@
+
+#include "funciones.h"
+
+typedef struct pares{int desplazamiento; int longitudLinea;} Pares;
+
+struct pcb {int ID;
+	    int* CODE, STACK, CursorSTACK;
+	    Pares indiceCodigo[];
+	    int* indiceEtiquetas;
+	    int* ProgramCounter;
+	    int TamanioContext;
+} ;
