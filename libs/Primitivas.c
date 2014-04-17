68400abafe952af1018be38ae2da4cf5d8918873
fedebonisconti <fedebonisconti26@gmail.com>
2014-04-16 21:24:00 -0300

    arreglada la funcion definirVariable

--- libs/Primitivas.c
+++ libs/Primitivas.c
@@ -9,19 +9,25 @@
 
 
 
-signed int* definirVariable(t_nombre_variable identificador_variable){ identificador_variable = malloc(sizeof(signed int));
-									if(identificador_variable*!=NULL){return identificador_variable*;}
-									else{ printf("No se puede reservar memoria");
-									return NULL*;
-												}
+signed int* definirVariable(t_nombre_variable identificador_variable){
+	identificador_variable = malloc(sizeof(signed int));
+	if(identificador_variable!=NULL){
+		return identificador_variable;
+	}
+	else{
+		printf("No se puede reservar memoria");
+		return NULL;
+	}
 }
 
 signed int* obtenerPosicionVariable(t_nombre_variable identificador_variable){
+	return 0;
 }
 
-signed int dereferenciar(signed int* direccion_variable){signed int aux; //extrae el valor de la variable ubicada en direccion_variable
-														 aux=*direccion_variable;
-														 return aux;
+signed int dereferenciar(signed int* direccion_variable){  //extrae el valor de la variable ubicada en direccion_variable
+	signed int aux;
+	aux=*direccion_variable;
+	return aux;
 }
 
 void asignar(signed int* direccion_variable, signed int valor){ //copia el valor en la variable ubicada en direccion_variable
@@ -29,9 +35,9 @@ void asignar(signed int* direccion_variable, signed int valor){ //copia el valor
 }
 
 signed int obtenerValorCompartida(t_nombre_compartida variable){ //deberia solicitar el valor de la variable al kernel
-
+	return 0;
 }
 
 signed int asignarValorCompartida(t_nombre_compartida variable, signed int valor){ //deberia solicitar al kernel que asigne valor a la variable
-
+	return 0;
 }
