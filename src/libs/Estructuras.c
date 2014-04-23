62a34cd14dd34121d2b20a8720dcf13f3f723586
fedebonisconti <fedebonisconti26@gmail.com>
2014-04-22 23:11:50 -0300

    agrego una estructura para el diccionario del indiceEtiquetas, cualquier cosa se puede cambiar

--- src/libs/Estructuras.c
+++ src/libs/Estructuras.c
@@ -1,18 +1,24 @@
 
 #include "funciones.h"
 #include "commons/collections/dictionary.h"
+#include "libs/commons/collections/node.h"
 
 typedef struct pares{
 		int desplazamiento;
 		int longitudLinea;
 } Pares;
 
+typedef struct {
+	t_hash_element **elements;
+	int elements_amount;
+} diccionario;
+
 typedef struct pcb {
 		int ID;
 	    int* CODE, STACK, CursorSTACK;
 	    //estaba int* indiceEtiquetas, pero por lo que dice la consigna: el indice de etiquetas constara de la serializacion
 	    //de un diccionario que ontiene el id de cada funcion o etiqueta...
-	    t_dictionary* indiceEtiquetas;
+	    diccionario* indiceEtiquetas;
 	    int* ProgramCounter;
 	    int TamanioContext;
 	    Pares indiceCodigo[];
