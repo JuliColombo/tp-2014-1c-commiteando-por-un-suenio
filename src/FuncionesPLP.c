882b6343ff634ef508c78e4a288a4f2604bae111
nicolasarmenti <utnso@utnso.(none)>
2014-04-25 19:54:15 -0300

    mostrarNodosPorPantallas

--- src/FuncionesPLP.c
+++ src/FuncionesPLP.c
@@ -28,26 +28,32 @@ void agregarAListaSegunPeso(t_programa programa, t_list* lista){
 	list_add_in_index(lista, position, &programa);
 }
 
+void mostrarNodosPorPantalla(t_list* lista){
+int i;
+int p;
+	printf("Estado de la cola:\n");
+	for (i=0;i<(list_size(lista));i++){
+		p=((t_programa*)(list_take(lista, i)->head->data))->peso;
+		printf(" Nodo %d   Peso %d\n", i, p); //Tal vez agregar el nombre del programa(? Preguntar
+	}
+}
 
 int main_plp(){
 	t_list* cola_programas=list_create();
 	printf("La cola está vacia");
 
 	//Cuando entra un programa nuevo
-	t_programa programa;
-	calcularPeso(programa);
-	char* literal="Abc"; //hay que ver como hacer para sacar un literal con el código
-	metadatada_desde_literal(literal);
-	if(solicitarMemoria(programa)){
-		agregarAListaSegunPeso(programa,cola_programas);
-		}
-		else{
-			printf("No hay memoria suficiente para agregar el programa");
-	}
-
-
-
-
+		t_programa programa;
+		calcularPeso(programa);
+		char* literal="Abc"; //hay que ver como hacer para sacar un literal con el código
+		metadatada_desde_literal(literal);
+		if(solicitarMemoria(programa)){
+			agregarAListaSegunPeso(programa,cola_programas);
+			}
+			else{
+				printf("No hay memoria suficiente para agregar el programa");
+			}
+		mostrarNodosPorPantalla(cola_programas);
 
 
 	return 0;
