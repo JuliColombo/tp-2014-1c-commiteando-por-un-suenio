70f54f5055839a62619092e6ac2a20fe8f1e2624
fedebonisconti <fedebonisconti26@gmail.com>
2014-05-16 19:50:43 -0300

    cambio en algunas funciones para agregar expresividad, siguen funcionando igual

--- Kernel/libs/funciones_PLP.c
+++ Kernel/libs/funciones_PLP.c
@@ -211,9 +211,9 @@ void vector_num(char** vector_string_num, int* config_valores, void* config_ids)
 }
 
 
-int cant_identificadores(char** config){
+int cant_identificadores(char** config_ids){
 	int i,a=0;
-	for(i=0;config[i]!=NULL;i++){
+	for(i=0;config_ids[i]!=NULL;i++){
 	a+=1;
 	}
 return a;
