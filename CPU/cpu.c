0983a9dd98a85d0d8e6e396c455ddfb65c058136
JuliColombo <julietanataliacolombo@yahoo.com.ar>
2014-05-26 18:19:38 -0300

    cambie algunas cositas

--- CPU/cpu.c
+++ CPU/cpu.c
@@ -18,27 +18,6 @@ int main (int argc, char **argv){
 	PATH=argv[1];
 	leerConfiguracion(PATH);
 
-	diccionario = dictionary_create();
-	pila = CREATE_STACK(1000);
-
-	definirVariable('a');
-	definirVariable('b');
-	definirVariable('c');
-
-	t_puntero a = obtenerPosicionVariable('a')+1;
-	t_puntero b = obtenerPosicionVariable('b')+1;
-	t_puntero c = obtenerPosicionVariable('c')+1;
-
-	asignar(a, 10);
-	asignar(b,-15);
-	asignar(c,9);
-
-	printf("para la variable a, tenemos el valor %d\n",dereferenciar(obtenerPosicionVariable('a')));
-	printf("para la variable b, tenemos el valor %d\n",dereferenciar(obtenerPosicionVariable('b')));
-	printf("para la variable c, tenemos el valor %d\n",dereferenciar(obtenerPosicionVariable('c')));
-
-
-
 
 	return 0;
 }
